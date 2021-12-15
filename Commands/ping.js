8bdc1a7d8d8a5b5c0245990331328300925ea019
olliemine <73561517+olliemine@users.noreply.github.com>
2021-12-14 20:41:26 -0600

    Cleanup

--- Commands/ping.js
+++ Commands/ping.js
@@ -5,20 +5,16 @@ const config = require("../Data/config.json");
 module.exports = new Command({
     name: "ping",
     description: "Bot status",
-
     async run(message, args, client) {
         const msg = await message.reply(`🏓Pong!`);
 
         const embed = new Discord.MessageEmbed()
         .setDescription(`
         Bot ping: ${client.ws.ping} ms
-        Discord API Latency: ${msg.createdTimestamp - message.createdTimestamp} ms
-        `)
+        Discord API Latency: ${msg.createdTimestamp - message.createdTimestamp} ms`)
         .setColor('AQUA')
         .setFooter(config.version);
 
         message.channel.send({ embeds: [embed] });
-        
-
     }
 }); 
